b078f1ae4384b17739b87d8cfccdf95678a1a266
tangjie <tangjie_mobile@163.com>
2016-01-26 12:50:31 +0800

    tabhostfragment  first commit
    tabhostfragment  first commit

---
+++ demo/src/main/java/com/tj/tabhostfragment/FragmentSale.java
@@ -0,0 +1,34 @@
+package com.tj.tabhostfragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class FragmentSale extends Fragment {
+	
+	@Override
+	public void onHiddenChanged(boolean hidden) {
+		super.onHiddenChanged(hidden);
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		LinearLayout ct = (LinearLayout) inflater.inflate(
+				R.layout.activity_tab_fragment_page, null);
+		TextView tx = (TextView) ct.findViewById(R.id.wait_tx);
+		tx.setText("sale");
+		
+		return ct;
+	}
+	
+}
